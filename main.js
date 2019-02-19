ec1740baac114cd6566294d9027e00a9ba4024cd
joeTheK <35149747+joeTheK@users.noreply.github.com>
2019-02-19 14:41:20 -0600

    Added Comments

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
- 
+//  creates an array with all the possible answers
  var answers = ["It is certain."," It is decidedly so.","Yes.","Signs point to yes.","Most likely.","Cannot predict now.","Ask again later.","My reply is no.","Don't count on it.","Very doubtful."];
-
+// makes the fuction that the button will call when clicked
  function answer(){
+    //  creates a variable as a placeholder for a random number
     let x = Math.floor(Math.random() * answers.length);
+    // grabs element with 'text' as id from documnet and changes it's inner html to answers[x]
     document.getElementById("text").innerHTML = answers[x];
  }
+
+// simple 8-ball code
